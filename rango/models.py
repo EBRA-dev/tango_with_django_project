a3869c9c28106873ccde24e047d2c79b3992e1ed
Ebou Saidy <2376797S@student.gla.ac.uk>
2021-02-03 16:06:50 +0000

    tenth commit

--- rango/models.py
+++ rango/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
+class category(models.Model):
+    name   = models.CharField(max_length = 128, unique = True)
+
+    def __str__(self):
+        return self.name
+
+class page(models.Model):
+    category = models.ForeignKey(category, on_delete = models.CASCADE)
+    title = models.CharField(max_length = 128)
+    url = models.URLField()
+    views = models.IntegerField(default = 0 )
+
+    def __str__(self):
+        return self.title
+
 # Create your models here.
